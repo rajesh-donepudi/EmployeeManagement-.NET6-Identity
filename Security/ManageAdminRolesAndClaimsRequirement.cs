26ad7cea1cd50eec29aebd7c704d0fbe767d3cd3
rajesh.donepudi <rajesh.donepudi@techigai.io>
2022-09-23 13:33:56 +0530

    initial commit

---
+++ Security/ManageAdminRolesAndClaimsRequirement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Security
+{
+    public class ManageAdminRolesAndClaimsRequirement : IAuthorizationRequirement
+    {
+        public override bool Equals(object obj)
+        {
+            return true;
+        }
+    }
+}
